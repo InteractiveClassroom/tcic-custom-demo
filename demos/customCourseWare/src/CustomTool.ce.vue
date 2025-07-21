0d0ef82ec823682a5fa3046c81d826d68ba28cd1
shuaisguo <shuaisguo@tencent.com>
2025-07-21 18:19:12 +0800

    feat: 添加 icon

--- demos/customCourseWare/src/CustomTool.ce.vue
+++ demos/customCourseWare/src/CustomTool.ce.vue
@@ -1,19 +1,30 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import img from './assets/Tool.svg'
+
 const handleClick = (item) => {
   alert('handleClick');
 }
 </script>
 
 <template>
-<div class="custom-tool" @click="handleClick">自定义工具</div>
+<div class="custom-tool" @click="handleClick">
+  <img class="tool-icon" :src="img"></img>自定义工具
+</div>
 </template>
 
 <style lang="less" scoped>
 .custom-tool {
   color: #fff;
-  font-size: 20px;
+  font-size: 16px;
   line-height: 35px;
   font-weight: 400;
+  display: flex;
+  .tool-icon {
+    width: 30px;
+    height: 30px;
+    background-size: 100%;
+    margin-right: 2px;
+  }
 }
 </style>
