e66cf363fd3610101d882732f9f2b04603ae4429
shuaisguo <shuaisguo@tencent.com>
2025-08-18 14:43:01 +0800

    chore: 默认不引入自定菜单

--- demos/customSmallClassLayout/src/main.js
+++ demos/customSmallClassLayout/src/main.js
@@ -1,12 +1,9 @@
 import { defineCustomElement } from 'vue';
 import SmallClassCustomLayout from './App.ce.vue';
-import CustomMenu from './CustomMenu.ce.vue';
 
 const MyVueElement = defineCustomElement(SmallClassCustomLayout, { shadowRoot: false });
 customElements.define('small-custom-layout', MyVueElement);
 
-const MyMenuElement = defineCustomElement(CustomMenu, { shadowRoot: false });
-customElements.define('custom-menu', MyMenuElement);
 TCICCustomUI.hooks(TCICCustomUI.THookType.Layout_BeforeInit).tap(() => {
   // 学生数大于 1 的纯视频小班课走自定义布局
   if (TCIC.SDK.instance.getClassInfo().maxRtcMember > 1 && TCIC.SDK.instance.isVideoOnlyClass()) {
